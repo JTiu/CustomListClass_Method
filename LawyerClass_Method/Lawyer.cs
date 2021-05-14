a08e4de8f0d88ecf704283d33b872d90366fa5c0
James Tiu <jamestiulawyer@gmail.com>
2021-05-14 19:14:52 -0400

    Add project files.

---
+++ LawyerClass_Method/Lawyer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LawyerClass_Method
+{
+    public class Lawyer
+    {
+        public string name;
+        public int yearsExperience;
+        public int trialsPerYear;
+        public int hearingsPerYear;
+
+        public int numberOfTrials;
+        public int numberOfHearings;
+
+        public Lawyer(string lawyerName, int totalNumberOfTrials, int totalNumberOfHearings)
+        {
+            name = lawyerName;
+            numberOfTrials = totalNumberOfTrials;
+            numberOfHearings = totalNumberOfHearings;
+        }
+
+        public void CheckInOffice()
+        {
+            Console.WriteLine($"{name} is in the office");
+        }
+        public int CareerTotalHearings()
+        {
+            numberOfHearings = yearsExperience * trialsPerYear;
+            Console.WriteLine($"{name} has a {numberOfHearings}");
+            return numberOfHearings;
+        }
+
+        public void CareerTotalTrials()
+        {
+            numberOfTrials = yearsExperience * trialsPerYear;
+            Console.WriteLine($"{name} has a {numberOfTrials}");
+        }
+
+        public void DisplayLawyerParameters()
+        {
+            Console.WriteLine($"{name}, {yearsExperience}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ LawyerClass_Method/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LawyerClass_Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Lawyer jamesTiuLawyer = new Lawyer("James", 3, 5000);
+            Console.WriteLine($"Facts: {na");
+            Console.ReadLine();
+        }
+    }
+}
