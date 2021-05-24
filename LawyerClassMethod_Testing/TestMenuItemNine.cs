60bfed8d3585c2a8f6a83a75ec281b9af876d1eb
James Tiu <jamestiulawyer@gmail.com>
2021-05-24 18:46:37 -0400

    Created test, item 9 and 10

---
+++ LawyerClassMethod_Testing/TestMenuItemNine.cs
@@ -0,0 +1,54 @@
+﻿using LawyerClass_Method;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace LawyerClassMethod_Testing
+{
+    [TestClass]
+    public class TestMenuItemNine
+    {
+        [TestMethod]
+        public void MenuItemNine_CheckZip_Capacity()//naming convbention 'method, underscore, expected result. whenever third item needed, add an extra underscore. nned to add areference.
+        { //arrange
+            CustomList<int> aList = new CustomList<int>();
+            aList.Add(1);
+            aList.Add(3);
+            aList.Add(5);
+            aList.Add(7);
+            aList.Add(9);
+
+            CustomList<int> bList = new CustomList<int>();
+            bList.Add(2);
+            bList.Add(4);
+            bList.Add(6);
+            bList.Add(8);
+            bList.Add(10);
+            CustomList<int> zList = aList.ZipMethod(bList);
+            //Assert
+
+            Assert.AreEqual(zList.Capacity, 10);
+        }
+        [TestMethod]
+        public void MenuItemNine_CheckZip_Index()//naming convbention 'method, underscore, expected result. whenever third item needed, add an extra underscore. nned to add areference.
+        { //arrange
+            CustomList<int> aList = new CustomList<int>();
+            aList.Add(1);
+            aList.Add(3);
+            aList.Add(5);
+            aList.Add(7);
+            aList.Add(9);
+
+            CustomList<int> bList = new CustomList<int>();
+            bList.Add(2);
+            bList.Add(4);
+            bList.Add(6);
+            bList.Add(8);
+            bList.Add(10);
+            CustomList<int> zList = aList.ZipMethod(bList);
+            //Assert
+
+            Assert.AreEqual(zList[0], 1);
+            Assert.AreEqual(zList[7], 8);
+        }
+    }
+}
---
+++ LawyerClassMethod_Testing/TestMenuItemTen.cs
@@ -0,0 +1,49 @@
+﻿using LawyerClass_Method;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace LawyerClassMethod_Testing
+{
+    [TestClass]
+    public class TestMenuItemTen
+    {
+        [TestMethod]
+        public void MenuItemTen_CheckSort_Capacity()//naming convbention 'method, underscore, expected result. whenever third item needed, add an extra underscore. nned to add areference.
+        { //arrange
+            CustomList<int> sortList = new CustomList<int>();
+            sortList.Add(99);
+            sortList.Add(33);
+            sortList.Add(55);
+            sortList.Add(44);
+            sortList.Add(22);
+            sortList.Add(11);
+            sortList.Add(66);
+            sortList.Add(88);
+            sortList.Add(77);
+            sortList.Add(61);
+            sortList.Add(76);
+            sortList.Add(69);
+            sortList.BubbleSort();
+          
+            //Assert
+
+            Assert.AreEqual(sortList.Capacity, 20);
+        }
+        [TestMethod]
+        public void MenuItemNine_CheckSort_Count()//naming convbention 'method, underscore, expected result. whenever third item needed, add an extra underscore. nned to add areference.
+        { //arrange
+            CustomList<int> sortList = new CustomList<int>();
+            sortList.Add(99);
+            sortList.Add(33);
+            sortList.Add(55);
+            sortList.Add(44);
+            sortList.Add(22);
+           
+            sortList.BubbleSort();
+
+            //Assert
+
+            Assert.AreEqual(sortList.Count, 0);
+        }
+    }
+}
